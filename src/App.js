e74f8d77b28a59a3578a7f094665fb50cd6c465e
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-02-27 11:03:56 -0300

    Criando HTML e CSS Base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './styles/global.css';
+import './contador.css'
 
-class App extends React.Component{
-  render(){
-    return <h1>Olá Mundo</h1>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="contador-titulo">0</h1>
+        <button type="button" className="contador-aumentar" >Aumentar</button>
+        <button type="button" className="contador-diminuir" >Diminuir</button>
+      </div>
+
+    );
   }
 }
 
