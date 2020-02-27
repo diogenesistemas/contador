b9c4f63fc0fbda4b14ec9d0b9abf6a30076d760d
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-02-27 12:03:05 -0300

    Criando Função de Aumentar e Diminuir

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import './styles/global.css';
 import './contador.css'
 
 class App extends React.Component {
+  state = {
+    numero: 0
+  }
+
+  aumentar = () => {
+    let novoNumero = this.state.numero;
+    novoNumero++;
+    this.setState({ numero: novoNumero });
+  }
+
+  diminuir = () => {
+    let novoNumero = this.state.numero;
+    if (novoNumero === 0) {
+      return alert("Hoje não Faro");
+    } else {
+      novoNumero--;
+      this.setState({ numero: novoNumero });
+    }
+
+
+
+  }
+
+
   render() {
     return (
       <div>
-        <h1 className="contador-titulo">0</h1>
-        <button type="button" className="contador-aumentar" >Aumentar</button>
-        <button type="button" className="contador-diminuir" >Diminuir</button>
+        <h1 className="contador-titulo">
+          {this.state.numero}
+        </h1>
+        <button type="button" className="contador-aumentar" onClick={this.aumentar}>Aumentar</button>
+        <button type="button" className="contador-diminuir" onClick={this.diminuir}>Diminuir</button>
       </div>
 
     );
