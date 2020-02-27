0aeb9efdcbce236d53a54b0bacb7095f8b438f82
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-02-27 12:17:45 -0300

    Refatorando e Desestruturando em Componentes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Contador from './componentes/contador'
 import './styles/global.css';
-import './contador.css'
 
-class App extends React.Component {
-  state = {
-    numero: 0
-  }
-
-  aumentar = () => {
-    let novoNumero = this.state.numero;
-    novoNumero++;
-    this.setState({ numero: novoNumero });
-  }
-
-  diminuir = () => {
-    let novoNumero = this.state.numero;
-    if (novoNumero === 0) {
-      return alert("Hoje não Faro");
-    } else {
-      novoNumero--;
-      this.setState({ numero: novoNumero });
-    }
-
-
-
-  }
-
-
-  render() {
-    return (
-      <div>
-        <h1 className="contador-titulo">
-          {this.state.numero}
-        </h1>
-        <button type="button" className="contador-aumentar" onClick={this.aumentar}>Aumentar</button>
-        <button type="button" className="contador-diminuir" onClick={this.diminuir}>Diminuir</button>
-      </div>
-
-    );
-  }
+class App extends Component {
+ render(){
+   return <Contador/>
+ }
 }
 
 export default App;
